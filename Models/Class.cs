37616762716dcb731dca29ca2600f58ac06cfdbf
Aravind Raj <aravinddevaraj777@gmail.com>
2019-02-25 16:23:17 +0530

    Initial commit

---
+++ Models/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TimeTable.Models
+{
+    public class Class
+    {
+        [Key]
+        public int ClassId { get; set; }
+
+        [Required(ErrorMessage = "Enter Class Name")]
+        public string ClassName { get; set; }
+
+        [Required(ErrorMessage = "Enter Class Venue")]
+        public string Venue { get; set; }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TimeTable.Models
+{
+    public class Course
+    {
+        [Required(ErrorMessage = "Enter Course Code")]
+        public string CourseCode { get; set; }
+
+        [Required(ErrorMessage = "Enter Course Name")]
+        public string CourseName { get; set; }
+
+    }
+}
