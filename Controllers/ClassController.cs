4ee02e8717a314a55e1b164902603fe8b8dc56e9
Aravind Raj <aravinddevaraj777@gmail.com>
2019-02-27 00:11:02 +0530

    Crud for class
    Aravind Raj M

---
+++ Controllers/ClassController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TimeTable.Models;
+using TimeTable.DataAccess;
+
+namespace TimeTable.Controllers
+{
+    public class ClassController : Controller
+    {
+        // GET: Class
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Class/
+        [HttpGet]
+        public ActionResult InsertClass()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertClass(Class c)
+        {
+            if (ModelState.IsValid) //checking model is valid or not
+            {
+                ClassDataAccess objDB = new ClassDataAccess();
+                //Console.WriteLine(c.ClassId.ToString());
+                ModelState.AddModelError("",c.ClassId.ToString());
+                string result = objDB.insertClass(c);
+                ViewData["result"] = result;
+                //ModelState.Clear(); //clearing model
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("", "Error in saving data");
+                return View();
+        }
+
+    }
+
+
+}
+}
--- DataAccess/ClassDataAccess.cs
+++ DataAccess/ClassDataAccess.cs
@@ -2,10 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using TimeTable.Models;
 
 namespace TimeTable.DataAccess
 {
     public class ClassDataAccess
     {
+        private SqlConnection sqlCon = null;
+        public ClassDataAccess()
+        {
+            //this.OpenConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+            this.OpenConnection(ConfigurationManager.ConnectionStrings["TimeTableConnection"].ToString());
+        }
+        public void OpenConnection(string conStr)
+        {
+            sqlCon = new SqlConnection { ConnectionString = conStr };
+            sqlCon.Open();
+        }
+        public void CloseConnection()
+        {
+            sqlCon.Close();
+        }
+        public string insertClass(Class c)
+        {
+            // Note the "placeholders" in the SQL query.  
+            string sql = "Insert Into Class(ClassId, ClassName, Venue) Values(@ClassId, @ClassName, @Venue)";
+            // This command will have internal parameters.  
+            string result = "";
+            using (SqlCommand command = new SqlCommand(sql, sqlCon))
+            {
+                // Fill params collection.    
+                SqlParameter parameter = new SqlParameter
+                {
+                    ParameterName = "@ClassId",
+                    Value = c.ClassId,
+                    SqlDbType = SqlDbType.Int
+                };
+                command.Parameters.Add(parameter);
+
+                parameter = new SqlParameter
+                {
+                    ParameterName = "@ClassName",
+                    Value = c.ClassName,
+                    SqlDbType = SqlDbType.Char,
+                    Size = 10
+                };
+                command.Parameters.Add(parameter);
+
+                parameter = new SqlParameter
+                {
+                    ParameterName = "@Venue",
+                    Value = c.Venue,
+                    SqlDbType = SqlDbType.Char,
+                    Size = 10
+                };
+                command.Parameters.Add(parameter);
+
+                //command.ExecuteNonQuery();
+                try
+                {
+                    result = command.ExecuteScalar().ToString();
+                }
+                catch(Exception e)
+                {
+
+                }
+            }
+            return result;
+        }
+
     }
 }
